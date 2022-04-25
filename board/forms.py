1001ad1c1d0683c557ddbbf76a07316eb4e30a6e
andy <andy.chiwon.kim@gmail.com>
2022-04-26 02:21:05 +0900

    wip

--- board/forms.py
+++ board/forms.py
@@ -1,5 +1,7 @@
+from email.policy import default
 from random import choice
 from re import A
+import re
 from django import forms
 from board.models import Card, Post
 
@@ -15,10 +17,34 @@ class CardForm(forms.ModelForm):
             'desc': forms.Textarea(attrs={'class':'form-control', 'rows':3}), 
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].widget.attrs.update({
+            'class': 'form-control',
+        })
+
+class Customclearable(forms.ClearableFileInput):
+    template_name='board/custom_clearable_file.html'
+
 class PostForm(forms.ModelForm):
+    image1_input = forms.ImageField(required=False, widget=Customclearable)
+    image2_input = forms.ImageField(required=False, widget=Customclearable)
+    image3_input = forms.ImageField(required=False, widget=Customclearable)
+    image4_input = forms.ImageField(required=False, widget=Customclearable)
+    image5_input = forms.ImageField(required=False, widget=Customclearable)
+    image6_input = forms.ImageField(required=False, widget=Customclearable)
+    image7_input = forms.ImageField(required=False, widget=Customclearable)
     class Meta:
         model = Post
-        fields = ['content', 'image1', 'image2', 'image3', 'tags', 'is_published']
+        fields = ['content', 'tags', 'is_published', 'image1_input', 'image2_input', 'image3_input', 'image4_input', 'image5_input', 'image6_input', 'image7_input']
         widgets = {
-            'is_published': forms.HiddenInput
+            'content': forms.Textarea(attrs={'rows':'4',}), 
+            'is_published': forms.HiddenInput,
         }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control',
+            })
--- board/views/post_view.py
+++ board/views/post_view.py
@@ -12,7 +12,6 @@ from board.models import Card, Post
 from board.forms import PostForm
 from django.contrib import messages
 
-
 class PostCreateView(CreateView):
     model = Post
     form_class = PostForm
@@ -35,17 +34,32 @@ class PostCreateView(CreateView):
                 raise PermissionDenied
 
     def form_valid(self, form):
+        print('started')
+        images = []
+        img_field_input = [form.cleaned_data['image1_input'], form.cleaned_data['image2_input'], form.cleaned_data['image3_input'], form.cleaned_data['image4_input'], form.cleaned_data['image5_input'], form.cleaned_data['image6_input'], form.cleaned_data['image7_input']]
+        print([form.cleaned_data['image1_input'], form.cleaned_data['image2_input'], form.cleaned_data['image3_input'], form.cleaned_data['image4_input'], form.cleaned_data['image5_input'], form.cleaned_data['image6_input'], form.cleaned_data['image7_input']])
+        for img in img_field_input:
+            if img != None: images.append(img)
+        print(images)
+        form.instance.num_images = len(images)
+        for i in range(len(images), 7):
+            images.append("")
+        print(images)
+        [form.instance.image1, form.instance.image2, form.instance.image3, form.instance.image4, form.instance.image5, form.instance.image6, form.instance.image7] = images
+        print([form.instance.image1, form.instance.image2, form.instance.image3, form.instance.image4, form.instance.image5, form.instance.image6, form.instance.image7])
         form.instance.author = self.request.user
         form.instance.card = get_object_or_404(
             Card, id=self.kwargs.get('card_id'))
         new_post = form.save(commit=False)
         new_post.save()
         form.save_m2m()
+        print('done')
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['card'] = get_object_or_404(Card, id=self.kwargs.get('card_id'))
+        context['num_images'] = 1
         return context
 
 
@@ -93,6 +107,14 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = 'board/post_create.html'
 
     def form_valid(self, form):
+        images = []
+        img_field_input = [form.cleaned_data['image1_input'], form.cleaned_data['image2_input'], form.cleaned_data['image3_input'], form.cleaned_data['image4_input'], form.cleaned_data['image5_input'], form.cleaned_data['image6_input'], form.cleaned_data['image7_input']]
+        for img in img_field_input:
+            if img != None and img != False: images.append(img)
+        form.instance.num_images = len(images)
+        for i in range(len(images), 7):
+            images.append("")
+        [form.instance.image1, form.instance.image2, form.instance.image3, form.instance.image4, form.instance.image5, form.instance.image6, form.instance.image7] = images
         form.instance.author = self.request.user
         newpost = form.save(commit=False)
         newpost.save()
@@ -108,4 +130,16 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['update'] = True
+        context['num_images'] = max(1, self.get_object().num_images)
         return context
+    
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['image1_input'] = self.object.image1
+        initial['image2_input'] = self.object.image2
+        initial['image3_input'] = self.object.image3
+        initial['image4_input'] = self.object.image4
+        initial['image5_input'] = self.object.image5
+        initial['image6_input'] = self.object.image6
+        initial['image7_input'] = self.object.image7
+        return initial
