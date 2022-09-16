826b048165392f9c9ac016ef0e6f7a46e3310b11
andy <andy.chiwon.kim@gmail.com>
2022-09-16 18:28:38 +0900

    test

--- board/views/post_view.py
+++ board/views/post_view.py
@@ -79,15 +79,18 @@ class PostCreateView(CreateView):
         form.instance.num_images = len(images)
         for i in range(len(images), 7):
             images.append("")
-        [form.instance.image1, form.instance.image2, form.instance.image3, form.instance.image4,
-            form.instance.image5, form.instance.image6, form.instance.image7] = images
+        # [form.instance.image1, form.instance.image2, form.instance.image3, form.instance.image4,
+        #     form.instance.image5, form.instance.image6, form.instance.image7] = images
+
+        [form.instance.image1s, form.instance.image2s, form.instance.image3s, form.instance.image4s,
+            form.instance.image5s, form.instance.image6s, form.instance.image7s] = images
 
         form.instance.author = self.request.user
         form.instance.card = get_object_or_404(Card, id=self.kwargs.get('card_id'))
         new_post = form.save(commit=False)
         new_post.save()
         form.save_m2m()
-        post_image_resize(new_post)
+        # post_image_resize(new_post)
         return redirect(self.get_success_url())
 
     def get_success_url(self) -> str:
@@ -175,14 +178,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         form.instance.num_images = len(images)
         for i in range(len(images), 7):
             images.append("")
-        [form.instance.image1, form.instance.image2, form.instance.image3, form.instance.image4,
-            form.instance.image5, form.instance.image6, form.instance.image7] = images
+        # [form.instance.image1, form.instance.image2, form.instance.image3, form.instance.image4,
+        #     form.instance.image5, form.instance.image6, form.instance.image7] = images
+
+        [form.instance.image1s, form.instance.image2s, form.instance.image3s, form.instance.image4s,
+            form.instance.image5s, form.instance.image6s, form.instance.image7s] = images
 
         form.instance.author = self.get_object().author
         rev_post = form.save(commit=False)
         rev_post.save()
         form.save_m2m()
-        post_image_resize(rev_post)
+        # post_image_resize(rev_post)
         
         redirect_url = redirect(self.get_success_url())
         if rev_post.content == '' and rev_post.num_images == 0:
